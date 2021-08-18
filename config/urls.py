bc3c2b19df29bf84a40e85cb99efa7c4387dd977
JOSEPH <josepholukunle1107@gmail.com>
2021-08-18 09:47:47 +0100

    Some debugging

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('resume.urls', namespace='resume')),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('resume.urls', namespace='resume')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
